3ff0bbf06dd6bc18ee7fd06c40580b9194b87ce9
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-02-05 14:06:11 -0500

    Getters and setters for Commission Employee

--- Module05/CommissionEmployee.java
+++ Module05/CommissionEmployee.java
@@ -1,3 +1,48 @@
-public class CommissionEmployee {
-    
+public class CommissionEmployee extends Employee {
+    private double grossSales;
+    private double commissionRate;
+
+    // TODO 5: Create constructor using super()
+    public CommissionEmployee(String firstName, String lastName,
+            String socialSecurityNumber,
+            double grossSales, double commissionRate) {
+
+        super(firstName, lastName, socialSecurityNumber);
+        this.setGrossSales(grossSales);
+        this.setCommissionRate(commissionRate);
+    }
+
+    // TODO 6: Create getters and setters with validation
+    public double getCommissionRate() {
+        return this.commissionRate;
+    }
+
+    public double getGrossSales() {
+        return this.grossSales;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate > 0 && commissionRate < 1) {
+            this.commissionRate = commissionRate;
+        } else {
+            throw new IllegalArgumentException(
+                    "Commission rate must be between 0 and 1");
+        }
+    }
+
+    public void setGrossSales(double grossSales) {
+        if (grossSales > 0.0) {
+            this.grossSales = grossSales;
+        } else {
+            throw new IllegalArgumentException(
+                    "Gross Sales must be greater than 0");
+        }
+    }
+
+    @Override
+    public double earnings() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'earnings'");
+    }
+
 }
