4c33255560311e4b74f637660b69abbee4c1ba56
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-02-05 13:59:56 -0500

    Employee class and constructor

---
+++ Module05/Employee.java
@@ -0,0 +1,14 @@
+public abstract class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    // TODO 1: Create a constructor
+    public Employee(String firstName, String lastName, String socialSecurityNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+}
