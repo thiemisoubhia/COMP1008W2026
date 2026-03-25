92e0ea13a00a5e7fe94073d29d3e6bae0733c718
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-03-25 15:40:19 -0400

    event handling for substraction

--- Lab05/CalculatorApp.java
+++ Lab05/CalculatorApp.java
@@ -51,11 +51,22 @@ public class CalculatorApp extends Application {
                 double num2 = Double.parseDouble(input2.getText());
                 resultLabel.setText("Result: " + (num1 + num2));
             } catch (Exception ex) {
-                resultLabel.setText("Error: Invalid input");
+                resultLabel.setText("Invalid input");
             }
         });
 
-        
+        //substraction
+        subBtn.setOnAction(e -> {
+            try {
+                double num1 = Double.parseDouble(input1.getText());
+                double num2 = Double.parseDouble(input2.getText());
+                resultLabel.setText("Result: "+ (num1 - num2));
+            } catch (Exception ex) {
+                resultLabel.setText("Invalid input");
+            }
+        });
+
+
         // Step 7: Create scene and show stage
 
     }
